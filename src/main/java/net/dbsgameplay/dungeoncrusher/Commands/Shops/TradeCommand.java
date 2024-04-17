053793eef4e9db5b0cdce2a3548ec7da5ba14225
HanneZHD <63670295+Hannes260@users.noreply.github.com>
2024-04-17 19:52:25 +0200

    Trade Command

---
+++ src/main/java/net/dbsgameplay/dungeoncrusher/Commands/Shops/TradeCommand.java
@@ -0,0 +1,13 @@
+package net.dbsgameplay.dungeoncrusher.Commands.Shops;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class TradeCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender,Command command, String label, String[] args) {
+
+        return false;
+    }
+}
