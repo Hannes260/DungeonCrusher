67a72c3b5cf167c36301960e41df427288365ece
deibe <deibertmaxim@gmail.com>
2024-08-22 14:03:39 +0200

    Ungefähren aufbau in Questcommand geschriben

--- src/main/java/net/dbsgameplay/dungeoncrusher/Commands/Quests/QuestCommand.java
+++ src/main/java/net/dbsgameplay/dungeoncrusher/Commands/Quests/QuestCommand.java
@@ -8,6 +8,15 @@ public class QuestCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
+        //Öffne ein Inventar mit 4 Quest types [Tutorial, Daily, Weekly, Monthly]
+        //In ner cfg speichern ob man die tutorial fertig hat um die anderen quest machenzu koönnen
+        //Enum mit QuestID - Quest- Beohnung
+        //Quest setzten
+        //Quest tracken
+        //Belohnung bekommen
+        //Quest Sperren
+        //Quest entsperren wenn neue kommt
+
         return false;
     }
 }
