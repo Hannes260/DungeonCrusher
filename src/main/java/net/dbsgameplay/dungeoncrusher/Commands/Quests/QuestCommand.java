08b046bc0a26f69aad5a6d98334cce2e0a012d8c
deibe <deibertmaxim@gmail.com>
2024-08-23 22:52:40 +0200

    Questmenü so weit es geht fertig.

--- src/main/java/net/dbsgameplay/dungeoncrusher/Commands/Quests/QuestCommand.java
+++ src/main/java/net/dbsgameplay/dungeoncrusher/Commands/Quests/QuestCommand.java
@@ -1,28 +1,28 @@
 package net.dbsgameplay.dungeoncrusher.Commands.Quests;
 
+import net.dbsgameplay.dungeoncrusher.utils.QuestBuilder;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 
 public class QuestCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
+        if (!(sender instanceof Player p)) return true;
         //Öffne ein Inventar mit 4 Quest types [Tutorial, Daily, Weekly, Monthly]
         //In ner cfg speichern ob man die tutorial fertig hat um die anderen quest machenzu koönnen
-        //Enum mit QuestID - Quest- Beohnung
+        //"Liste" mit QuestID - Quest- Beohnung
         //Quest setzten
         //Quest tracken
         //Belohnung bekommen
         //Quest Sperren
         //Quest entsperren wenn neue kommt
 
-
-
-
-
+        QuestBuilder.fillQuestmenü(p);
+        p.openInventory(QuestBuilder.getQuestmenü());
         return false;
     }
 }
--- src/main/java/net/dbsgameplay/dungeoncrusher/utils/QuestBuilder.java
+++ src/main/java/net/dbsgameplay/dungeoncrusher/utils/QuestBuilder.java
@@ -1,19 +1,83 @@
 package net.dbsgameplay.dungeoncrusher.utils;
 
+import net.dbsgameplay.dungeoncrusher.DungeonCrusher;
+import net.dbsgameplay.dungeoncrusher.utils.Configs.LocationConfigManager;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class QuestBuilder {
 
+    public QuestBuilder(DungeonCrusher dungeonCrusher, LocationConfigManager locationConfigManager) {
+        this.dungeonCrusher = dungeonCrusher;
+        this.locationConfigManager = locationConfigManager;
+    }
+    private LocationConfigManager locationConfigManager;
+    private static DungeonCrusher dungeonCrusher;
+    public static Inventory questMenu = Bukkit.createInventory(null, 54, "§7Questmenü");
 
     public static Inventory getQuestmenü() {
-        Inventory questMenu = Bukkit.createInventory(null, 54, "§7Questmenü");
         return questMenu;
     }
 
-    public static void fillQuestmenü() {
+    public static void fillQuestmenü(Player player) {
+        //tutorial
+        //hinzufügen wenn das Tutorial abgeschlossen ist
+        ItemStack tutorialStack = new ItemStack(Material.GRASS_BLOCK);
+        ItemMeta tutorialMeta = tutorialStack.getItemMeta();
+        tutorialMeta.setItemName("§aTutorial");
+        tutorialMeta.setLore(null);
+        if (dungeonCrusher.getConfig().getBoolean("xxxIDontKnowxxx")) {
+            tutorialMeta.setEnchantmentGlintOverride(true);
+        }else {
+            tutorialMeta.setEnchantmentGlintOverride(false);
+        }
+        tutorialStack.setItemMeta(tutorialMeta);
+        questMenu.setItem(9, tutorialStack);
 
+        //Daily
+        //hinzufügen wenn die Daily abgeschlossen ist
+        ItemStack dailyStack = new ItemStack(Material.CLOCK);
+        ItemMeta dailylMeta = dailyStack.getItemMeta();
+        dailylMeta.setItemName("§6Daily");
+        dailylMeta.setLore(null);
+        if (dungeonCrusher.getConfig().getBoolean("daily." + player.getUniqueId().toString())) {
+            dailylMeta.setEnchantmentGlintOverride(true);
+        }else {
+            dailylMeta.setEnchantmentGlintOverride(false);
+        }
+        dailyStack.setItemMeta(dailylMeta);
+        questMenu.setItem(11, dailyStack);
+
+        //Weekly
+        //hinzufügen wenn die Weekly abgeschlossen ist
+        ItemStack weeklyStack = new ItemStack(Material.CLOCK);
+        ItemMeta weeklylMeta = weeklyStack.getItemMeta();
+        weeklylMeta.setItemName("§6Daily");
+        weeklylMeta.setLore(null);
+        if (dungeonCrusher.getConfig().getBoolean("weekly." + player.getUniqueId().toString())) {
+            weeklylMeta.setEnchantmentGlintOverride(true);
+        }else {
+            weeklylMeta.setEnchantmentGlintOverride(false);
+        }
+        weeklyStack.setItemMeta(weeklylMeta);
+        questMenu.setItem(13, weeklyStack);
+
+        //Month
+        //hinzufügen wenn die Month abgeschlossen ist
+        ItemStack monthlyStack = new ItemStack(Material.CLOCK);
+        ItemMeta monthlylMeta = monthlyStack.getItemMeta();
+        monthlylMeta.setItemName("§6Daily");
+        monthlylMeta.setLore(null);
+        if (dungeonCrusher.getConfig().getBoolean("monthly." + player.getUniqueId().toString())) {
+            monthlylMeta.setEnchantmentGlintOverride(true);
+        }else {
+            monthlylMeta.setEnchantmentGlintOverride(false);
+        }
+        monthlyStack.setItemMeta(weeklylMeta);
+        questMenu.setItem(15, weeklyStack);
     }
 }
