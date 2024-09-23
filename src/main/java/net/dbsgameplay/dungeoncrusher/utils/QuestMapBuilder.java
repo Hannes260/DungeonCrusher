1ea347d87d9f83b1b7f726e436495b8e749e167e
ditomax <96945825+DitomaxHD@users.noreply.github.com>
2024-09-23 11:55:02 +0200

    Update QuestMapBuilder.java
    .

--- src/main/java/net/dbsgameplay/dungeoncrusher/utils/QuestMapBuilder.java
+++ src/main/java/net/dbsgameplay/dungeoncrusher/utils/QuestMapBuilder.java
@@ -7,8 +7,9 @@ public class QuestMapBuilder {
     public static void BuildMap() {
         HashMap<String, String> tutorialQuestMap = QuestBuilder.tutorialQuestMap;
 
-        tutorialQuestMap.put("t3", "§6Verbessere dein Schwert auf Lvl. 2.");
-        tutorialQuestMap.put("t2", "§6Kaufe dir Essen im Teleporter.");
-        tutorialQuestMap.put("t1", "§6Trinke einen Stärketrank.");
+        tutorialQuestMap.put("t4", "§6Verbessere dein Schwert auf Lvl. 2.");
+        tutorialQuestMap.put("t3", "§6Kaufe dir Essen im Teleporter.");
+        tutorialQuestMap.put("t2", "§6Trinke einen Stärketrank.");
+        tutorialQuestMap.put("t1", "§6Erreiche Ebene 2.");
     }
 }
