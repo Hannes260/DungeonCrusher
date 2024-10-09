f4729ac7bdc2ee1d0e1fb900b677a382153b4882
Maxim <deibertmaxim@gmail.com>
2024-10-09 19:29:34 +0200

    Weekly update fix

--- src/main/java/net/dbsgameplay/dungeoncrusher/utils/quests/Weekly.java
+++ src/main/java/net/dbsgameplay/dungeoncrusher/utils/quests/Weekly.java
@@ -91,11 +91,11 @@ public class Weekly {
     }
 
     public static void checkForOrginQuestUpdate() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EE:HH:mm:ss");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEEEE:HH:mm:ss");
         LocalDateTime now = LocalDateTime.now();
         String time = now.format(formatter);
 
-        if (time.substring(0, time.length() - 3).equalsIgnoreCase("01:00:01") && (Integer.parseInt(time.substring(9)) >= 00 && Integer.parseInt(time.substring(9)) <= 10)) {
+        if (time.substring(0, time.length() - 3).equalsIgnoreCase("Monday:00:01") && (Integer.parseInt(time.substring(9)) >= 00 && Integer.parseInt(time.substring(9)) <= 10)) {
             String k1 = null;
             String k2 = null;
             String k3 = null;
